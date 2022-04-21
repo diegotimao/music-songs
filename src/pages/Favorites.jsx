2d741f80202777b5bf02a21abee230da0a8a8d1e
Diego Cavalcanti <diegotimao1104@gmail.com>
2022-04-21 18:44:04 -0300

    Correção de erros

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,57 +1,12 @@
 import React from 'react';
-import MusicCard from '../components/MusicCard';
-import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 import Header from '../components/Header';
 
 class Favorites extends React.Component {
-  constructor() {
-    super();
-
-    this.getFavorits = this.getFavorits.bind(this);
-
-    this.state = {
-      loading: false,
-    };
-  }
-
-  componentDidMount() {
-    this.getFavorits();
-  }
-
-  async getFavorits() {
-    this.setState({
-      loading: true,
-    });
-    const responseFavorits = await getFavoriteSongs();
-    this.setState({
-      loading: false,
-      result: responseFavorits,
-      musics: responseFavorits,
-    });
-  }
-
   render() {
-    const {
-      loading,
-      musics,
-      result,
-    } = this.state;
-
     return (
       <div data-testid="page-favorites">
         <Header />
-        { loading ? <strong>Carregando</strong> : (
-          musics.map((item) => (
-            <MusicCard
-              key={ item.trackName }
-              previewUrl={ item.previewUrl }
-              name={ item.trackName }
-              trackId={ item.trackId }
-              result={ result }
-            />
-          ))
-        )}
       </div>
     );
   }
--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class ProfileEdit extends React.Component {
+  constructor() {
+    super();
+
+    this.profileUser = this.profileUser.bind(this);
+
+    this.state = {
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.profileUser();
+  }
+
+  async profileUser() {
+    this.setState({
+      loading: true,
+    });
+
+    const responseUser = await getUser();
+    this.setState({
+      loading: false,
+    });
+    console.log(responseUser);
+  }
+
   render() {
+    const {
+      loading,
+    } = this.state;
     return (
       <div data-testid="page-profile-edit">
         <Header />
-        <h1>ProfileEdit</h1>
+        { loading ? <strong>Carregando...</strong> : null }
       </div>
     );
   }
